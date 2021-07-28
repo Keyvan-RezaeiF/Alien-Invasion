d9215514f9879356462735ce07072bf21e754a22
Keyvan-RezaeiF <k1rzfi80@gmail.com>
2021-07-28 18:55:38 +0430

    move the aliens right

--- alien_invasion.py
+++ alien_invasion.py
@@ -39,6 +39,7 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self._update_bullets()
+            self._update_aliens()
             self._update_screen()
 
     def _check_events(self):
@@ -113,6 +114,10 @@ class AlienInvasion:
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
+
+    def _update_aliens(self):
+        """Update the positions of all aliens in the fleet."""
+        self.aliens.update()
 
     def _update_screen(self):
         # Update images on the screen, and flip to the new screen.
